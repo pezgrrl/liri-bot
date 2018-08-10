0e600c86d141f36d2be9c05222cc94537b711b2d
Jill Morimoto <jillmorimoto@gmail.com>
2018-08-09 20:36:51 -0700

    second

--- liri.js
+++ liri.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
 
 //Add the code required to import the `keys.js` file and store it in a variable.
 
-var spotify = new Spotify(keys.spotify);
+var keys = require('./keys.js');
 var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
+var name = process.argv[3];
 
 /*
 Make it so liri.js can take in one of the following commands:
@@ -16,41 +22,80 @@ Make it so liri.js can take in one of the following commands:
     * `do-what-it-says`
     ????????????????
     */
-if(command == "movie this") {
-    moviethis();
+if (command == 'movie-this') {
+    console.log(command);
+    moviethis(name);
+
+}
+
+
+function moviethis(movieName) {
+
+    request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+
+            console.log("* Title of the movie:         " + JSON.parse(body).Title);
+            console.log("* Year the movie came out:    " + JSON.parse(body).Year);
+            console.log("* IMDB Rating of the movie:   " + JSON.parse(body).imdbRating);
+            console.log("* Country produced:           " + JSON.parse(body).Country);
+            console.log("* Language of the movie:      " + JSON.parse(body).Language);
+            console.log("* Plot of the movie:          " + JSON.parse(body).Plot);
+            console.log("* Actors in the movie:        " + JSON.parse(body).Actors);
+        }
+    });
+
 }
 
 function mytweets() {
-//This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+    var params = {
+        screen_name: "NotARus36809644",
+        count: 20
+    };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets[0].text);
+        }
+    });
+    console.log(' ');
+    console.log('Last 20 Tweets:')
+
+    // for (var i = 0; i < tweets.length; i++) {
+    //     var number = i + 1;
+    //     console.log(" ");
+    // }
 }
+var spotifyThisSong = function (songName) {
 
+    if (songName === undefined) {
+        songName = "the sign ace of base";
+    }
 
-function spotifythissong(song_name) {
-/*This will show the following information about the song in your terminal/bash window
-     
-Artist(s)
-The song's name
-A preview link of the song from Spotify
-The album that the song is from
-If no song is provided then your program will default to "The Sign" by Ace of Base.
-*/
-}
+    spotify.search({
+        type: 'track',
+        query: songName
+    }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + err);
 
+        } else {
+            for (var i = 0; i < data.tracks.items[0].artists.length; i++) {
+                if (i === 0) {
+                    console.log("Artist(s):     " + data.tracks.items[0].artists[i].name);
+                } else {
+                    console.log("               " + data.tracks.items[0].artists[i].name);
+                }
+                console.log("Song:          " + data.tracks.items[0].artists[i].name);
+                console.log("Preview Link:  " + data.tracks.items[0].artists[i].preview_url);
+                console.log("Album:         " + data.tracks.items[0].artists[i].album.name);
+            }
 
-function moviethis(movie_name) {
-    //You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-    /*This will output the following information to your terminal/bash window:
-
-       Title of the movie.
-       Year the movie came out.
-       IMDB Rating of the movie.
-       Rotten Tomatoes Rating of the movie.
-       Country where the movie was produced.
-       Language of the movie.
-       Plot of the movie.
-       Actors in the movie.
-If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-*/
+        };
+    })
 }
 
 function dowhatitsays() {
@@ -59,4 +104,8 @@ function dowhatitsays() {
      Feel free to change the text in that document to test out the feature for other commands.
      */
 }
+if (command == 'my-tweets') {
+    console.log(command);
+    mytweets();
 
+}
